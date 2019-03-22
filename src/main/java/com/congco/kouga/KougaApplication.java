b3191fe55f0421d93da9505455dd45b3234bf0f5
zhengcong <mxlyzc@163.com>
2019-03-22 11:33:54 +0800

    feat(all): #init

---
+++ src/main/java/com/congco/kouga/KougaApplication.java
@@ -0,0 +1,13 @@
+package com.congco.kouga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KougaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KougaApplication.class, args);
+    }
+
+}
